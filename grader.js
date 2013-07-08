1e31351df3e258923a40445b6e458da739da6019
kbr239 <kraftery7@gmail.com>
2013-07-08 20:55:29 +0000

    Updated to accept url, but debugging necessarry

--- grader.js
+++ grader.js
@@ -26,6 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
+var util = require('util');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -44,8 +46,7 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function($, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -61,14 +62,39 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var buildfn = function(checks, outputJson) {
+    var urlCheck = function(result) {
+	if (result instanceof Error) {
+            console.error('Error: ' + util.format(result.message));
+        } 
+	else {
+	    $ = cheerio.load(result);
+	    outputJson(checkHtmlFile($, checks));
+	}
+    };
+    return urlCheck;
+};
+
+var outputJson = function(checkJson) {
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        .option('-u, --url <url_address>', 'heroku url')
+	.parse(process.argv);
+    
+    if(program.url) {
+	 var urlCheck = buildfn(program.checks, outputJson);
+	 rest.get(program.url).on('complete', urlCheck);
+    }
+    else {
+	$ = cheerioHtmlFile(program.file);
+	outputJson(checkHtmlFile($, program.checks));
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
